c0aa89cb473cb1a00e7453e594d7c32905ec0ea4
Paul Bovbel <pbovbel@locusrobotics.com>
2018-06-06 22:19:53 -0400

    Try to run pull_distro on CI

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='tailor-distro',
+    packages=['tailor_distro'],
+    version='0.0.0',
+    description='Build rosdistro bundles',
+    license='Proprietary',
+    author='Paul Bovbel',
+    author_email='pbovbel@locusrobotics.com',
+    url='https://github.com/locusrobotics/tailor-distro',
+    install_requires=[
+        'rosdistro',
+        'PyYaml'
+    ],
+    entry_points={
+      'console_scripts': [
+          'tailor_distro = tailor_distro.pull_distro:main'
+      ]
+    }
+)
---
+++ tailor_distro/pull_distro.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+import pathlib
+import rosdistro
+import subprocess
+import yaml
+
+# from rosinstall_generator.generator import ARG_ALL_PACKAGES, generate_rosinstall
+
+def main():
+    rosdistro_index = pathlib.Path("rosdistro/index.yaml").resolve().as_uri()
+    os.environ["ROSDISTRO_INDEX_URL"] = rosdistro_index
+
+
+    index = rosdistro.get_index(rosdistro_index)
+    distro = rosdistro.get_distribution(index, "locus")
+
+    repositories = {}
+    for repo in distro.repositories.items():
+        repositories[repo[0]] = {
+            'type': repo[1].source_repository.type,
+            'url': repo[1].source_repository.url,
+            'version': repo[1].source_repository.version
+        }
+
+    repositories_file = pathlib.Path('catkin.repos')
+    repositories_file.write_text(yaml.dump({'repositories': repositories}))
+
+    workspace_dir = pathlib.Path("workspace/src")
+    try:
+        workspace_dir.mkdir()
+    except FileExistsError:
+        pass
+
+    subprocess.check_call(["vcs", "import", str(workspace_dir), "--input", str(repositories_file)])
+
+if __name__ == '__main__':
+    main()
