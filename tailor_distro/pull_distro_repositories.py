dbefd0d9682b9d9cab79abad8714d2e95d8ef55a
Paul Bovbel <pbovbel@locusrobotics.com>
2018-06-11 23:02:45 -0400

    Stash bundle contents

--- tailor_distro/pull_distro_repositories.py
+++ tailor_distro/pull_distro_repositories.py
@@ -8,6 +8,9 @@ import yaml
 def main():
     # TODO(pbovbel) make args
     rosdistro_index = pathlib.Path("tailor-distro/rosdistro/index.yaml").resolve().as_uri()
+    workspace_dir = pathlib.Path("workspace/src")
+    repositories_file = workspace_dir / pathlib.Path('catkin.repos')
+    # TODO(end)
 
     index = rosdistro.get_index(rosdistro_index)
     distro = rosdistro.get_distribution(index, "locus")
@@ -20,14 +23,12 @@ def main():
             'version': repo[1].source_repository.version
         }
 
-    repositories_file = pathlib.Path('catkin.repos')
-    repositories_file.write_text(yaml.dump({'repositories': repositories}))
-
-    workspace_dir = pathlib.Path("workspace/src")
     try:
         workspace_dir.mkdir(parents=True)
     except FileExistsError:
         pass
+
+    repositories_file.write_text(yaml.dump({'repositories': repositories}))
 
     subprocess.check_call(["vcs", "import", str(workspace_dir), "--input", str(repositories_file)])
     subprocess.check_call(["vcs", "pull", str(workspace_dir)])
