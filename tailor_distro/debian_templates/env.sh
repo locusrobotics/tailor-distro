d5b6be313424929c0f6d3c385e0ed974aea80f48
Paul Bovbel <pbovbel@locusrobotics.com>
2018-06-14 17:26:49 -0400

    Workaround env.sh issue

---
+++ tailor_distro/debian_templates/env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# Created by tailor-distro as workaround for https://github.com/colcon/colcon-ros/issues/16
+
+if [ $# -eq 0 ] ; then
+  /bin/echo "Usage: env.sh COMMANDS"
+  /bin/echo "Calling env.sh without arguments is not supported anymore. Instead spawn a subshell and source a setup file manually."
+  exit 1
+fi
+
+# ensure to not use different shell type which was set before
+CATKIN_SHELL=sh
+
+# source setup.sh from same directory as this file
+_CATKIN_SETUP_DIR=$(cd "`dirname "$0"`" > /dev/null && pwd)
+. "$_CATKIN_SETUP_DIR/setup.sh"
+
+export ROS_WORKSPACE=$_CATKIN_SETUP_DIR
+
+exec "$@"
