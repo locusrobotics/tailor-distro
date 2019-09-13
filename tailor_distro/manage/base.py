7b3e0503b31462efa3ccfb540c158645e4b942c9
David V. Lu <davidvlu@gmail.com>
2019-09-13 15:28:56 -0400

    Centralizing parsing logic

--- tailor_distro/manage/base.py
+++ tailor_distro/manage/base.py
@@ -4,14 +4,13 @@ import click
 import difflib
 import github
 import json
-import os
 import re
 import pathlib
 import yaml
 
 from urllib.parse import urlsplit, urlunsplit
-from urllib import HTTPError
-from rosdistro import get_index, get_index_url, get_distribution, Index
+from urllib.error import HTTPError
+from rosdistro import get_index, get_distribution, Index
 from rosdistro.distribution_file import create_distribution_file
 from rosdistro.writer import yaml_from_distribution_file
 
@@ -39,60 +38,9 @@ def insert_auth_token(url, token):
 RAW_GH_PATTERN = re.compile('https://raw.githubusercontent.com/(?P<repo>[^/]+/[^/]+)/(?P<branch>[^/]+)/(?P<path>.+)')
 
 
-def _load_github_helper(url):
-    m = RAW_GH_PATTERN.match(url)
-    if not m:
-        raise RuntimeError('Cannot parse raw github url: {}'.format(url))
-    gh = get_github_client()
-    repo = gh.get_repo(m.group('repo'))
-    return repo, m.group('path'), m.group('branch')
-
-
-def load_github_file(url):
-    repo, path, branch = _load_github_helper(url)
-    git_file = repo.get_contents(path, branch)
-    yaml_str = git_file.decoded_content.decode()
-    return yaml.safe_load(yaml_str)
-
-
-def write_github_file(url, new_contents, message='[no message given]'):
-    repo, path, branch = _load_github_helper(url)
-    git_file = repo.get_contents(path, branch)
-    original_contents = git_file.decoded_content.decode()
-
-    print('\n'.join(difflib.unified_diff(original_contents.split('\n'), new_contents.split('\n'), lineterm='',
-                                         fromfile=path, tofile='%s (modified)' % path)))
-
-    response = ''
-    try:
-        while not response or response[0] not in 'yn':
-            response = input('Make commit "{}"? (y/n): '.format(message)).strip().lower()
-    except KeyboardInterrupt:
-        print()
-        response = 'n'
-    except EOFError:
-        print()
-        response = 'n'
-
-    if response[0] == 'n':
-        print('Maybe later.')
-        return
-
-    repo.update_file(path, message, new_contents, git_file.sha, branch=branch)
-
-
-def get_private_index(internal_index_path):
+def get_distro_from_index(index, dist_name, type_='distribution'):
     """
-       Version of rosdistro.get_index that can load from a private repository
-    """
-    data = load_github_file(internal_index_path)
-    base_url = os.path.dirname(internal_index_path)
-    return Index(data, base_url)
-
-
-def get_private_distro(index, dist_name, type_='distribution'):
-    """
-        Version of rosdistro.get_distribution that can load from a private repository
+       Portion of the logic from rosdistro.get_distribution
     """
     if dist_name not in index.distributions.keys():
         valid_names = ', '.join(sorted(index.distributions.keys()))
@@ -100,15 +48,8 @@ def get_private_distro(index, dist_name, type_='distribution'):
     dist = index.distributions[dist_name]
     if type_ not in dist.keys():
         raise RuntimeError('unknown release type "%s"' % type_)
-    url = dist[type_]
 
-    if not isinstance(url, list):
-        data = load_github_file(url)
-    else:
-        data = []
-        for u in url:
-            data.append(load_github_file(u))
-    return create_distribution_file(dist_name, data)
+    return dist[type_]
 
 
 class BaseVerb(metaclass=abc.ABCMeta):
@@ -116,27 +57,61 @@ class BaseVerb(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def execute(self, rosdistro_path, distro):
-        self.rosdistro_path = rosdistro_path
-        index_path = rosdistro_path / 'rosdistro' / 'index.yaml'
-        if index_path.exists():
-            internal_index_path = index_path.resolve().as_uri()
-        else:
-            internal_index_path = get_index_url()
+        m = RAW_GH_PATTERN.match(rosdistro_path)
+        if m:
+            gh = get_github_client()
+            self.repo = gh.get_repo(m.group('repo'))
+            self.path = m.group('path')
+            self.branch = m.group('branch')
 
-        try:
+            self.rosdistro_path = pathlib.Path(self.path)
+
+            try:
+                self.internal_index = get_index(rosdistro_path)
+                self.internal_distro = get_distribution(self.internal_index, distro)
+            except HTTPError:
+                # Only needed for private github repositories
+                distro_folder = self.rosdistro_path.parent
+                self.internal_index = Index(self.load_distro_file(self.path), str(distro_folder))
+
+                dist_url = get_distro_from_index(self.internal_index, distro)
+                if not isinstance(dist_url, list):
+                    data = self.load_distro_file(dist_url)
+                else:
+                    data = []
+                    for u in dist_url:
+                        data.append(self.load_distro_file(str(u)))
+                self.internal_distro = create_distribution_file(distro, data)
+        else:
+            self.repo = None
+
+            self.rosdistro_path = pathlib.Path(rosdistro_path)
+            if not self.rosdistro_path.exists():
+                if 'http' in rosdistro_path:
+                    raise RuntimeError(f'Cannot use non-Github path ({rosdistro_path}) at this time.')
+                else:
+                    raise RuntimeError(f'Cannot parse rosdistro_path ({rosdistro_path} as github path or '
+                                       'find it as local file.')
+            internal_index_path = (rosdistro_path / 'rosdistro' / 'index.yaml').resolve().as_uri()
             self.internal_index = get_index(internal_index_path)
             self.internal_distro = get_distribution(self.internal_index, distro)
-        except HTTPError:
-            self.internal_index = get_private_index(internal_index_path)
-            self.internal_distro = get_private_distro(self.internal_index, distro)
 
         self.internal_distro_file = self.internal_index.distributions[distro]['distribution'][-1]
+
+    def load_distro_file(self, path):
+        if self.repo:
+            git_file = self.repo.get_contents(path, self.branch)
+            yaml_str = git_file.decoded_content.decode()
+        else:
+            path_obj = self.rosdistro_path / path
+            yaml_str = path_obj.open()
+        return yaml.safe_load(yaml_str)
 
     def register_arguments(self, parser):
         parser.set_defaults(verb=self.execute)
         parser.add_argument('--distro', required=True, help="Distribution on which to operate")
         # TODO(pbovbel) Use path relative to package?
-        parser.add_argument('--rosdistro-path', type=pathlib.Path, default='.', help="Index URL override")
+        parser.add_argument('--rosdistro-path', default='.', help="Index URL override")
 
     def repositories_arg(self, parser):
         parser.add_argument('repositories', nargs='*', metavar='REPO', help="Repositories to operate on")
@@ -146,7 +121,7 @@ class BaseVerb(metaclass=abc.ABCMeta):
         parser.add_argument('--upstream-index', help="Upstream index URL override")
 
     def load_upstream(self, distro, upstream_index, upstream_distro):
-        recipes = yaml.safe_load((self.rosdistro_path / 'config' / 'recipes.yaml').open())
+        recipes = self.load_distro_file('config/recipes.yaml')
         try:
             info = recipes['common']['distributions'][distro]['upstream']
         except KeyError:
@@ -157,12 +132,32 @@ class BaseVerb(metaclass=abc.ABCMeta):
             upstream_distro if upstream_distro is not None else info['name']
         )
 
-    def write_internal_distro(self):
+    def write_internal_distro(self, message='[no message]'):
         new_contents = yaml_from_distribution_file(self.internal_distro)
 
-        m = RAW_GH_PATTERN.match(self.internal_distro_file)
-        if not m:
+        if self.repo:
+            git_file = self.repo.get_contents(self.path, self.branch)
+            original_contents = git_file.decoded_content.decode()
+
+            print('\n'.join(difflib.unified_diff(original_contents.split('\n'), new_contents.split('\n'), lineterm='',
+                                                 fromfile=self.path, tofile='%s (modified)' % self.path)))
+
+            response = ''
+            try:
+                while not response or response[0] not in 'yn':
+                    response = input('Make commit "{}"? (y/n): '.format(message)).strip().lower()
+            except KeyboardInterrupt:
+                print()
+                response = 'n'
+            except EOFError:
+                print()
+                response = 'n'
+
+            if response[0] == 'n':
+                print('Maybe later.')
+                return
+
+            self.repo.update_file(self.path, message, new_contents, git_file.sha, branch=self.branch)
+        else:
             distro_file_path = pathlib.Path(self.internal_distro_file[len('file://'):])
             distro_file_path.write_text(new_contents)
-        else:
-            write_github_file(self.internal_distro_file, new_contents)
