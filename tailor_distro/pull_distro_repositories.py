2426ef1b7139daf0853819c33ffd9756e954662a
Bianca Bendris <bbendris@locusrobotics.com>
2025-12-02 15:40:04 +0100

    Rename repository folders

--- tailor_distro/pull_distro_repositories.py
+++ tailor_distro/pull_distro_repositories.py
@@ -8,6 +8,7 @@ import tarfile
 import glob
 import shutil
 import github
+import os
 
 from requests.exceptions import HTTPError
 from catkin_pkg.package import parse_package
@@ -178,6 +179,18 @@ def process_repo(repo: str, tarball_url: str, base_dir: pathlib.Path) -> None:
             sleep(RETRY_WAIT_SECONDS)
     with tarfile.open(archive_path) as tar:
         tar.extractall(path=repo_dir)
+
+    # Rename the extracted dir as the repository name without any sha
+    extracted_dir = None
+    extracted_dir = next(
+        name for name in os.listdir(repo_dir)
+        if os.path.isdir(os.path.join(repo_dir, name))
+    )
+    if extracted_dir is not None:
+        os.rename(
+            os.path.join(repo_dir, extracted_dir),
+            os.path.join(repo_dir, repo),
+        )
 
 
 def pull_repositories(
